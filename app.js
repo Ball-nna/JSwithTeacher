a8664f2e91b81719e1f6d80fe8b2b6354516f2b1
Ball-nna <minasszz1234@gmail.com>
2022-01-11 17:41:26 +0700

    update

--- app.js
+++ app.js
@@ -3,3 +3,8 @@
 //alert("แน่นะว่านายใจเกเร")
 //alert("งั้นมาเข้าชมรมกันเถอะ")
 console.log("helloworld")
+function play() {
+    var audio = document.getElementById("audio");
+    
+    audio.play();
+  }
