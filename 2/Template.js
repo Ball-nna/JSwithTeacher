d03ee1706e75b74e58dc58521ed4781827989360
Ball-nna <minasszz1234@gmail.com>
2022-01-18 14:09:14 +0700

    update

---
+++ 2/Template.js
@@ -0,0 +1,12 @@
+console.log("\n***** Concatenation with template literals *****\n")
+let str1 = "JavaScript"
+let str2 = "fun"
+
+ console.log('i am writing code in ${str1}')
+ console.log('Formattiog in ${str1} is ${str2}!')
+
+ console.log("\n***** Expreesion with template literals *****\n")
+
+ let bool1 = true
+ console.log('1+1 is ${1+1}')
+ console.log("the opposite of ture is ${!bool1}")
