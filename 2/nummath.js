95abca5b5c605df72d3662eb22ceae318ccb8df9
Ball-nna <minasszz1234@gmail.com>
2022-01-18 14:51:05 +0700

    update

---
+++ 2/nummath.js
@@ -0,0 +1,16 @@
+let num1 = 100
+console.log("******Basic Math******")
+console.log(num1 + 25)
+console.log(num1 - 100)
+console.log(num1.toFixed(2) * 100)
+console.log(num1 / 100)
+
+
+console.log("******Additional Mathmatocal Operations******")
+console.log(num1 % 1500)
+console.log(++num1)
+console.log(--num1)
+
+console.log("******Using the Math object******")
+console.log(Math.PI.toFixed(2))
+console.log(Math.sqrt(num1))
