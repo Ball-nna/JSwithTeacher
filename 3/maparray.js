247dc307dc4076107a3ec4396b35bba004a3a613
Ball-nna <minasszz1234@gmail.com>
2022-01-25 13:51:12 +0700

    updatenew

---
+++ 3/maparray.js
@@ -0,0 +1,5 @@
+const finalParticipants = ["Taylor","Donald","Don","Natasha","Bobby"]
+const announcement = finalParticipants.map((member)=>{
+    return member+"Joined the contest"
+})
+console.log(announcement)
